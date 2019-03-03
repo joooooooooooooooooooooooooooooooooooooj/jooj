f27086f33a496213a1dd7c6249b0b7ccd6838edb
eddie <appositum@protonmail.com>
2019-03-03 02:41:54 -0300

    Move tests and add random testing
    - Removed the individual tests on Spec.hs and created new individual files for them.
    - Random `Value` testing for parsing and unparsing

--- test/Spec.hs
+++ test/Spec.hs
@@ -31,69 +31,20 @@ parseFiles path = listDirectory path >>= traverse (parseFile . (path++))
 
 main :: IO ()
 main = hspec $ do
-  describe "Parse JSON" $ do
-    it "parse test files" $ do
+  describe "JooJ" $ do
+    it "can parse the test files" $ do
       res <- parseFiles "test/files/"
       res `shouldSatisfy` all isJust
 
-    it "can parse formatted" $ do
-      parse x `shouldSatisfy` isJust
-
-    it "can parse inline string" $ do
-      parse y `shouldSatisfy` isJust
-
-    it "can parse object inside object" $ do
-      parse z `shouldSatisfy` isJust
-
     it "can parse scientific notation" $ do
-      lst <- sample' (arbitrary @Scientific)
-      parse . show <$> lst `shouldSatisfy` all isJust
+      xs <- sample' (arbitrary @Scientific)
+      map (parse . show) xs `shouldSatisfy` all isJust
 
-x :: String
-x = [r|{
-  "booleano": true,
-  "color" :"#82b92c",
-  "nulo": null,
-  "fodases": {
-    "iai": "parsero",
-    "nao": "ligo",
-    "kk": -1232
-  }
-}
-|]
+    it "can parse strings" $ do
+      xs <- sample' (arbitrary @String)
+      map (parse . show) xs `shouldSatisfy` all isJust
 
-y :: String
-y = [r|{"booleano": true, "color": "#82b92c", "nulo": null, "fodase" : {"iai":213123, "kk": null}}|]
-
-z :: String
-z = [r|
-{
-  "array": [
-    1,
-    2,
-    3
-  ],
-  "boolean": true,
-  "color": "#82b92c",
-  "null": null,
-  "number": 123,
-  "another": 12345.055,
-  "object": {
-    "a": "b",
-    "c": "d",
-    "e": "f"
-  },
-  "string": "Hello World",
-
-  "oq":{
-    "0": {
-      "neurons": "300",
-      "weights": [1,2,3,4,5],
-      "scisci": {
-        "one" :2.5e-3,
-        "twooo":   -1.3e-3
-      }
-    }
-  }
-  }
-|]
+    -- Guarantees the AST is the same before and after the unparsing
+    it "can unparse and parse back" $ do
+      xs <- sample' (arbitrary @Value)
+      map (parse . unparseJson) xs `shouldSatisfy` all isJust
