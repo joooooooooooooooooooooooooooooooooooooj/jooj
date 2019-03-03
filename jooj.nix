b1d73758b6e51b4aac72e69fc61f6da374c3837c
eddie <appositum@protonmail.com>
2019-03-02 22:47:42 -0300

    Arbitrary testing using QuickCheck for scientific numbers
    Use QuickCheck to test parsing on randomly generated scientific numbers.
    `scientific` was added to the test suit dependencies
    `quickcheck-instances` was added to the test suit and library dependencies for the `Arbitrary` instance for `Scientific`

--- jooj.nix
+++ jooj.nix
@@ -1,12 +1,12 @@
-{ mkDerivation, base, containers, directory, hspec, megaparsec, raw-strings-qq, scientific, stdenv }:
+{ mkDerivation, base, containers, directory, hspec, megaparsec, QuickCheck, quickcheck-instances, raw-strings-qq, scientific, stdenv }:
 mkDerivation {
   pname = "jooj";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = false;
-  libraryHaskellDepends = [ base containers directory megaparsec scientific ];
-  testHaskellDepends = [ base directory hspec megaparsec raw-strings-qq ];
+  libraryHaskellDepends = [ base containers directory megaparsec QuickCheck quickcheck-instances scientific ];
+  testHaskellDepends = [ base directory hspec megaparsec QuickCheck quickcheck-instances raw-strings-qq scientific ];
   doHaddock = false;
   homepage = "https://github.com/appositum/jooj#readme";
   license = stdenv.lib.licenses.asl20;
