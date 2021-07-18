21131f36810262c51b19f1677971cc924a8236ca
appositum <appositum@protonmail.com>
2021-07-18 08:17:17 -0300

    LTS-18.2 stack resolver, disable nix

--- jooj.nix
+++ jooj.nix
@@ -11,6 +11,6 @@ mkDerivation {
   libraryHaskellDepends = [ base containers megaparsec scientific ];
   testHaskellDepends = [ base directory hspec megaparsec QuickCheck quickcheck-instances raw-strings-qq scientific ];
   doHaddock = false;
-  homepage = "https://github.com/appositum/jooj#readme";
+  homepage = "https://github.com/joooooooooooooooooooooooooooooooooooooj/jooj#readme";
   license = lib.licenses.asl20;
 }
